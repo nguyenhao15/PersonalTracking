94ece036401c6550e6d69c89aac1da870ebe98cb
nguyenhao <lynguyenhao15@gmail.com>
2026-03-26 17:22:17 +0700

    feat: replace spring boot with nestjs and restructure monorepo

--- backend/src/main/java/com/haro/_5/repositories/DebtRepo.java
+++
@@ -1,7 +0,0 @@
-package com.haro._5.repositories;
-
-import com.haro._5.models.Debt;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DebtRepo extends MongoRepository<Debt, String> {
-}
